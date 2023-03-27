94e83a6d930a08b15b0193471d3e733db3f6345b
Pirasanthan Jesugeevegan <pirasanthanjesugeevegan@Pirasanthans-MacBook-Pro.local>
2023-03-27 09:37:06 +0100

    setting up cucumber to the project

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,31 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const fs = require('fs-extra');
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.amazon.co.uk/',
+    specPattern: '**/*.feature',
+    video: false,
+    videoUploadOnPasses: false,
+    retries: 1,
+    chromeWebSecurity: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 40000,
+    pageLoadTimeout: 40000,
+    responseTimeout: 40000,
+    multiple: true,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    restartBrowserBetweenSpecFiles: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('file:preprocessor', cucumber());
+      on('after:run', (results) => {
+        if (results) {
+          fs.mkdirSync('cypress/.run', { recursive: true });
+          fs.writeFile('cypress/.run/results.json', JSON.stringify(results));
+        }
+      });
     },
   },
 });
